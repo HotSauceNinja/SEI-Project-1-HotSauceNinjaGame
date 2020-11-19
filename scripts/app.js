8b12d1e706f16ab000cea954a81db41f2f134400
Sandra Spighel <s.spighel@gmail.com>
2020-11-19 16:19:43 +0000

    Game grid finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,40 @@
 function init() {
 
-  console.log('JS is up and running again!')
+  // VARIABLES
+  const grid = document.querySelector('.grid')
+  
+  const width = 20
+  const cellCount = width * width
+  const cells = []
+
+  // FUNCTIONS
+
+  // Creating the Gameplay Area
+  function createGrid() {
+    // Creating the cells and adding them on the board
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.setAttribute('class', 'grid-div') // setting class to each cell -> space them out evenly & highlight borders
+      cell.textContent = i // todo To count the cells for easier navigation during game development
+      grid.appendChild(cell) // placing cell on grid
+      cells.push(cell) // pushing each cell into the cells array
+    }
+  }
+
+  
+  
+  
+  // CALLING THE FUNCTIONS
+  createGrid() // To create the Gameplay Area
+
 
 
 
 }
 
 window.addEventListener('DOMContentLoaded', init)
+
+// Explaining what following code does
+// todo Developer notes, code to be commented out before submitting project
+// ! Important note
+// ? Can also use this colour for something
