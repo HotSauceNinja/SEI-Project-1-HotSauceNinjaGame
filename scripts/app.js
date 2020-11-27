354b93759a3d33f901a9a52e272049175ab63bc8
Sandra Spighel <s.spighel@gmail.com>
2020-11-27 14:22:21 +0000

    with sound on

--- scripts/app.js
+++ scripts/app.js
@@ -118,11 +118,11 @@ function init() {
       //   audio.play()
       //   break
       case 13: // Shoot with enter key
-        audio.src = './sound/throwHotSauce.mp3'
+        audio.src = '../sound/throwHotSauce.mp3'
         audio.play()
         break
       case 32: // Shoot with space key
-        audio.src = './sound/throwHotSauce.mp3'
+        audio.src = '../sound/throwHotSauce.mp3'
         audio.play()
         break
       default:
@@ -131,7 +131,7 @@ function init() {
   }
 
   function playAudioStart () {
-    audio.src = './sound/gong.mp3'
+    audio.src = '../sound/gong.mp3'
     audio.play()
   }
 
@@ -273,42 +273,42 @@ function init() {
 
     if (positionOfFood.contains('drumstickEven')) {
       score = score + 100
-      audio.src = './sound/foodHit.mp3'
+      audio.src = '../sound/foodHit.mp3'
       audio.play()
 
     } else if (positionOfFood.contains('drumstickOdd')) {
       score = score + 100
-      audio.src = './sound/foodHit.mp3'
+      audio.src = '../sound/foodHit.mp3'
       audio.play()
 
     } else if (positionOfFood.contains('eggEven')) {
       score = score + 200
-      audio.src = './sound/foodHit.mp3'
+      audio.src = '../sound/foodHit.mp3'
       audio.play()
 
     } else if (positionOfFood.contains('eggOdd')) {
       score = score + 200
-      audio.src = './sound/foodHit.mp3'
+      audio.src = '../sound/foodHit.mp3'
       audio.play()
 
     } else if (positionOfFood.contains('friesEven')) {
       score = score + 300
-      audio.src = './sound/foodHit.mp3'
+      audio.src = '../sound/foodHit.mp3'
       audio.play()
 
     } else if (positionOfFood.contains('friesOdd')) {
       score = score + 300
-      audio.src = './sound/foodHit.mp3'
+      audio.src = '../sound/foodHit.mp3'
       audio.play()
 
     } else if (positionOfFood.contains('pizzaEven')) {
       score = score + Math.floor(Math.random() * 500)
-      audio.src = './sound/foodHit.mp3'
+      audio.src = '../sound/foodHit.mp3'
       audio.play()
 
     } else if (positionOfFood.contains('pizzaOdd')) {
       score = score + Math.floor(Math.random() * 500)
-      audio.src = './sound/foodHit.mp3'
+      audio.src = '../sound/foodHit.mp3'
       audio.play()
   
     } else {
@@ -459,7 +459,7 @@ function init() {
 
       // if forkPosition includes ninjaClass remove both items and decrease ninjaLives with 1
       if (cells[forkPosition].classList.contains(ninjaClass)) {
-        audio.src = './sound/ninjaHit.mp3'
+        audio.src = '../sound/ninjaHit.mp3'
         audio.play()
         removeItemFromGrid(forkPosition)
         removeNinja(forkPosition)
@@ -688,7 +688,7 @@ function init() {
       gameOverText.innerHTML = 'You won, you swift, spicy lightning!'
       displayGameOverBox()
     }
-    audio.src = './sound/endBell.mp3'
+    audio.src = '../sound/endBell.mp3'
     audio.play()
   }
 
